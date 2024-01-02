4a394e5c5d520f44313ba633954f1bf39fdec69c
Jason-Lee3 <Jasoneugenelee@gmail.com>
2024-01-01 21:29:09 -0800

    first commit on pc

--- index.js
+++ index.js
@@ -1,51 +1,60 @@
 const myLibrary = [];
-const display = [];
+
+
+
 function Book(title, author, numPages, read) {
     // the constructor...
     this.title = title;
     this.author = author;
     this.numPages = numPages;
-    this.read = read;
+    if (read == "yes") 
+        this.read = true
+    else {
+        this.read = false
+    }
   }
   
-  function addBookToLibrary() {
-    // do stuff here
-    const newTitle = prompt('new book title');
-    const newAuthor = prompt('new book author');
-    let newBook = new Book(newTitle, newAuthor);
-    myLibrary.push(newBook);
-  }
+Book.prototype.toggleRead = function() {
+    this.read = !this.read;
+}
+
+function toggleRead(index) {
+    myLibrary[index].toggleRead();
+    displayLibrary();
+    
+}
 
 function displayLibrary() {
     const div = document.querySelector(".card-container");
+    div.innerHTML = "";
     for (let i = 0; i < myLibrary.length; i++) {
-        if (!display.includes(myLibrary[i])) {
-            div.innerHTML += `
+        div.innerHTML += `
             <div class="card">
                 <div class="card-text">
                 <h3>${myLibrary[i].title}</h3>
                 <p>${myLibrary[i].author}</p>
                 <p>${myLibrary[i].numPages}</p>
-                <p>${myLibrary[i].read}</p>
+                <p id="book-read" style="${myLibrary[i].read ? "background-color: red" : "background-color: blue"}">${myLibrary[i].read ? "Read" : "Not Read Yet"}</p>
                 </div>
                 <div class="button-div">
-                    
+                    <button class="card-btn" onclick="removeBook(${i})">Remove</button>
+                    <button class="card-btn" onclick="toggleRead(${i})">Read</button>
                 </div>
+                
             </div>
             `
-            display.push(myLibrary[i]);
-        }
     }
 }
 
-const btn = document.querySelector(".form-submit");
+const btn = document.querySelector("#form-post");
 
 const bookTitle = document.querySelector("#title");
 const bookAuthor = document.querySelector("#author");
 const numPages = document.querySelector("#numPages");
 const read = document.querySelector("#read");
 
-btn.addEventListener("click", (event) => {
+btn.addEventListener("submit", (event) => {
+    
     let newBook = new Book(bookTitle.value, bookAuthor.value, numPages.value, read.value);
     myLibrary.push(newBook);
     displayLibrary();
@@ -54,19 +63,14 @@ btn.addEventListener("click", (event) => {
     numPages.value = "";
     read.value = "";
     event.preventDefault();
+
 })
 
-let removeBtn = document.createElement("button");
-removeBtn.classList.add("remove");
 
-
-removeBtn.addEventListener("click", (event) => {
-    let temp = document.querySelector(".button-div");
-    temp.appendChild(removeBtn);
-    console.log(event.target.parentElement.parentElement);
-    event.target.parentElement.parentElement.remove();
-})
-
+function removeBook(index) {
+    myLibrary.splice(index, 1);
+    displayLibrary();
+}
 
 
 
